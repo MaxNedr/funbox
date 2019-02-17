b6994cea2b9443a8e3fb83f9587387abbc2f9c1d
NedrNout <maxim.nedozrelov@ya.ru>
2019-02-17 22:27:56 +0300

    эфекты наведения и пр.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ window.onload = function () {
 
     const item1 = document.getElementById('item1');
     const weight1 = document.getElementById('weight_item1');
+    const header_item1 = document.getElementById('header_item1');
     item1.onmouseover = function () {
         if (item1.hasAttribute('selected')) {
             item1.classList.add('bc-p_hover');
@@ -40,14 +41,22 @@ window.onload = function () {
             item1.classList.remove('bc-p');
             item1.classList.add('bc-b');
             weight1.classList.remove('bc-p_hover');
-            weight1.classList.add('bc-p');
+            weight1.classList.add('bc-b');
+            header_item1.classList.remove('color_pink');
+            header_item1.classList.add('color_666');
+            header_item1.innerText = 'Сказочное заморское яство'
         } else {
             item1.setAttribute('selected', '');
             item1.classList.remove('bc-b_hover');
             item1.classList.remove('bc-b');
             weight1.classList.remove('bc-b_hover');
+            weight1.classList.remove('bc-b');
             weight1.classList.add('bc-p');
             item1.classList.add('bc-p');
+            header_item1.classList.remove('color_666');
+            header_item1.classList.add('color_pink');
+            header_item1.innerText = 'Котэ не одобряет?'
+
         }
 
 
@@ -90,12 +99,13 @@ window.onload = function () {
             item2.classList.remove('bc-p');
             item2.classList.add('bc-b');
             weight2.classList.remove('bc-p_hover');
-            weight2.classList.add('bc-p');
+            weight2.classList.add('bc-b');
         } else {
             item2.setAttribute('selected', '');
             item2.classList.remove('bc-b_hover');
             item2.classList.remove('bc-b');
             weight2.classList.remove('bc-b_hover');
+            weight2.classList.remove('bc-b');
             weight2.classList.add('bc-p');
             item2.classList.add('bc-p');
         }
@@ -134,18 +144,19 @@ window.onload = function () {
 
     };
     item3.addEventListener('click', function () {
-        if (item3.hasAttribute('selected')){
+        if (item3.hasAttribute('selected')) {
             item3.removeAttribute('selected');
             item3.classList.remove('bc-p_hover');
             item3.classList.remove('bc-p');
             item3.classList.add('bc-b');
             weight3.classList.remove('bc-p_hover');
-            weight3.classList.add('bc-p');
-        } else{
+            weight3.classList.add('bc-b');
+        } else {
             item3.setAttribute('selected', '');
             item3.classList.remove('bc-b_hover');
             item3.classList.remove('bc-b');
             weight3.classList.remove('bc-b_hover');
+            weight3.classList.remove('bc-b');
             weight3.classList.add('bc-p');
             item3.classList.add('bc-p');
         }
